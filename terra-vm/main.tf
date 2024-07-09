73473b21aba141080fc858fcb1049b9719f6d369
yasser <mac@macs-MacBook-Pro.local>
2024-07-09 12:16:11 +0100

    The base line of all branch
    This branch will contains the common file between all branch

---
+++ terra-vm/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "digitalocean_ssh_key" "default" {
+  name       = "mac_pub_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "digitalocean_droplet" "vm" {
+  image  = "centos-stream-9-x64"
+  name   = "vm"
+  region = "fra1"
+  size   = "s-2vcpu-2gb"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
+}
+
+output "vm_ip" {
+  value = digitalocean_droplet.vm.ipv4_address
+}
